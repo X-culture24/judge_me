2021c2a0a338ca1a95b386678fde802f4630c310
X-culture24 <arthurarcade42@gmail.com>
2025-05-25 20:32:23 +0300

    updated scoreboard to user display name

--- public/scoreboard_data.php
+++ public/scoreboard_data.php
@@ -9,7 +9,7 @@ foreach ($users as $participant) {
     $highlight = $rank <= 3 ? "highlight-$rank" : '';
     echo '<tr class="' . $highlight . '">';
     echo '<td>' . $rank++ . '</td>';
-    echo '<td>' . htmlspecialchars($participant['name']) . '</td>';
+    echo '<td>' . htmlspecialchars($participant['display_name']) . '</td>';
     echo '<td>' . $participant['total_score'] . '</td>';
     echo '</tr>';
 }
